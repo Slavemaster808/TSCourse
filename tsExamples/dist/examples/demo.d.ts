f4f51331c5e8422d971a200938c49c0509e2a4fb
Slavemaster808 <mega.kaker@mail.ru>
2025-09-07 13:33:08 +0700

    Delete build files

--- tsExamples/dist/examples/demo.d.ts
+++
@@ -1,7 +0,0 @@
-import { User, Product } from '../types';
-export declare const users: User[];
-export declare const products: Product[];
-export declare const demonstrateConditionals: () => void;
-export declare const demonstrateLoops: () => void;
-export declare const demonstrateGenerics: () => void;
-//# sourceMappingURL=demo.d.ts.map
